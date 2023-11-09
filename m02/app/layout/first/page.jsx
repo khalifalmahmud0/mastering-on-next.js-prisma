e922c0d6fa9d50ebfecf6f538dcb5db6164b73ad
Khalif AL Mahmud <khalif@Khalifs-MacBook-Pro.local>
2023-11-10 02:06:39 +0600

    module 02 | QA: Any way to specefy Layout Name for a specefic Page ?

---
+++ m02/app/layout/first/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Page = () => {
+    return (
+        <>
+            <p>{'Layout > First > Page'}</p>
+        </>
+    );
+};
+
+export default Page;
---
+++ m02/app/layout/second/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Page = () => {
+    return (
+        <>
+           <p>{'layout > second > page.jsx'}</p>
+        </>
+    );
+};
+
+export default Page;
