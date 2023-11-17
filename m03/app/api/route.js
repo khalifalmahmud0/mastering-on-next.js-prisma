59162219bc7867a6e624d533ad7dd6835bb92eb8
Khalif AL Mahmud <kam@Khalifs-MacBook-Pro.local>
2023-11-18 01:33:54 +0600

    Practice Cookie hander from module 03

--- m03/app/api/route.js
+++ m03/app/api/route.js
@@ -1,5 +1,6 @@
 import { NextResponse } from "next/server";
 import { headers } from "next/headers";
+import {console} from "next/dist/compiled/@edge-runtime/primitives";
 export const GET = async (req, res) => {
 	// Way to get Query Params from Request
 	let { searchParams } = new URL(req.url);
@@ -19,17 +20,26 @@ export const POST = async (req, res) => {
 	// formdata
 	// let formDataValues = await req.formData();
 	// let company = formDataValues.get("company");
+
 	// Way to get body data from Request
 	// let bodyData = await req.json(); // receive data from request body
+
+	// Header Data
 	let headersData = headers();
 	console.log(headersData);
 	let name = headersData.get("name");
+
+	// Cookie
+	const cookiePractice = req.cookies;
+
+
 	return NextResponse.json(
 		{
 			status: 204,
 			// body:bodyData,
 			// formData: company,
 			head: name,
+			cookie: cookiePractice.get('pass')
 		},
 		{ status: 201 } // manual status code send
 	);
