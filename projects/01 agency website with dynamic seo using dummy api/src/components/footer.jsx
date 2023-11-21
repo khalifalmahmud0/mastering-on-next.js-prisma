5e6e93b5d319ee0000e0c327a689e284bae0fe26
Khalif AL Mahmud <kam@Khalifs-MacBook-Pro.local>
2023-11-22 00:34:11 +0600

    Create Header Footer Component

---
+++ projects/01 agency website with dynamic seo using dummy api/src/components/footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <>
+            <h1>This is Footer Componet</h1>
+        </>
+    );
+};
+
+export default Footer;
---
+++ projects/01 agency website with dynamic seo using dummy api/src/components/header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+    return (
+        <>
+            <h1>
+                This is Header Content
+            </h1>
+        </>
+    );
+};
+
+export default Header;
