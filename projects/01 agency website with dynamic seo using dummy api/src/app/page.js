e4b12087d78f05954987e0b57a44b81080942dd5
khalifalmahmud0 <code.khalif@gmail.com>
2023-11-22 13:25:57 +0600

    Add Testimonial

--- projects/01 agency website with dynamic seo using dummy api/src/app/page.js
+++ projects/01 agency website with dynamic seo using dummy api/src/app/page.js
@@ -1,9 +1,13 @@
 import Hero from "@/components/hero";
+import Team from "@/components/team";
+import Testimonial from "@/components/testimonial";
 
 export default function Home() {
     return (
         <>
             <Hero/>
+            <Team/>
+            <Testimonial/>
         </>
     );
 }
